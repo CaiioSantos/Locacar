2d61c027621d8c9f817d70a3ae6d5bdea0201e04
CaiioSantos <caiohgs13@gmail.com>
2019-11-16 02:17:48 -0300

    finalizando o fluxo de ClientePF faltando apenas a parte da UI

--- src/com/company/Controller/ControllerClientePF.java
+++ src/com/company/Controller/ControllerClientePF.java
@@ -1,18 +1,31 @@
 package com.company.Controller;
 
 
-import com.company.Exception.ClienteException.ClientePfException;
-import com.company.Exception.RepositoryClienteException;
+import com.company.Exception.ClientePFException;
+import com.company.Exception.ClientePFException.ClientePfException;
+import com.company.Exception.RepositoryClientePFException;
 import com.company.Model.ClientePf;
-import com.company.Repository.IRepositoryClientePF;
+import com.company.Repository.RepositoryClientePF;
 
 
 import java.util.List;
 
 public class ControllerClientePF implements IControllerClientePF {
-      private IRepositoryClientePF repositoryClientePF;
+    private static ControllerClientePF instancia;
+      private RepositoryClientePF repositoryClientePF = RepositoryClientePF.getInstance();
+
+    public ControllerClientePF() throws  RepositoryClientePFException, ClientePFException {
+    }
+    public static ControllerClientePF getInstance() throws ClientePFException, RepositoryClientePFException{
+          if (instancia == null){
+              instancia = new ControllerClientePF();
+          }
+        return instancia;
+      }
+
+
     @Override
-    public void inserirClientePF(ClientePf clientePf) throws ClientePfException, RepositoryClienteException {
+    public void inserirClientePF(ClientePf clientePf) throws ClientePfException, RepositoryClientePFException {
           this.repositoryClientePF.inserirClientePF(clientePf);
     }
 
