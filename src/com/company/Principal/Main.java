f9cd97ce2d168d30f99b1ba301f66d7f69bfe4ee
CaiioSantos <caiohgs13@gmail.com>
2019-11-30 00:10:25 -0300

    Atualizando

--- src/com/company/Principal/Main.java
+++ src/com/company/Principal/Main.java
@@ -10,80 +10,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    Scanner scanner = new Scanner(System.in);
+
+
 
     public static void main(String[] args) throws ClientePFException.ClientePfException, RepositoryClientePFException, ClientePFException {
 
-
-
-        Scanner input = new Scanner(System.in);
-        int sair = 0;
-        int opcao =0;
-        do {
-            System.out.println("Digite 1 para cadastrar cliente");
-            opcao = input.nextInt();
-            switch (opcao) {
-                case 1:
-                    cadastrarCLiente();
-                case 2:
-        }
-        }while (opcao !=0);
-    }
-    private static void cadastrarCLiente( ) throws ClientePFException.ClientePfException, RepositoryClientePFException, ClientePFException {
-        Facade facade = new Facade();
-        ClientePF clientePF = new ClientePF();
-        Scanner input = new Scanner(System.in);
-        System.out.println("Digite 1 para Cadastrar CLiente\n Digite 2 para lista Cliente\n Digite 3 para deletar veiculo");
-        int opcao;
-        do {
-            opcao = input.nextInt();
-            switch (opcao) {
-                case 1:
-                    System.out.println("Digite o nome do cliente a ser adicionado");
-                    clientePF.setNome(input.next());
-                    System.out.println("Digite o CPF do cliente a ser adicionado");
-                    clientePF.setCpf(input.next());
-                    System.out.println("Digite o CPF do cliente a ser adicionado");
-                    clientePF.setEmail(input.next());
-                    try {
-                        facade.inserirClientePF(clientePF);
-                    }catch ( RepositoryClientePFException e){
-                        System.out.println("alguma coisa");
-                    }
-                    break;
-                case 2:
-                    facade.listarCLientePF(clientePF);
-                    break;
-                case 3:
-                    facade.deletarClientePF(clientePF);
-                case 4:
-                    facade.updateClientePF(clientePF);
-            }
-        }while (opcao !=0);
-
-    }
-}
-
-
-
-
-
-
-  /*  private static void menuCadastro() throws ClientePFException, RepositoryClientePFException {
-        int opcao;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Escolha uma das opções  \n1 - Cadastrar CLiente Pessoa Fisica \n2 Cadastrar Veiculo ");
-        opcao = scanner.nextInt();
-
-        switch (opcao){
-            case 1:
-                UiCarro uiCarro = new UiCarro();
-                uiCarro.showMenu();
-            case 2:
-                break;
     }
 
 }
-}*/
 
 
